1521625c6a81876d52f719a0b8d0c1c25e34ce72
Max <maxigor.ferreira@gmail.com>
2022-06-06 18:53:34 -0300

    github.sh

---
+++ github.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+#-------------------------------------------------------------------------
+#      _          _    __  __      _   _
+#     /_\  _ _ __| |_ |  \/  |__ _| |_(_)__
+#    / _ \| '_/ _| ' \| |\/| / _` |  _| / _|
+#   /_/ \_\_| \__|_||_|_|  |_\__,_|\__|_\__|
+#  Arch Linux Post Install Setup and Config
+#-------------------------------------------------------------------------
+
+echo
+echo "INSTALLING SOFTWARE FROM GITHUB"
+echo
+
+
+git clone https://github.com/maxigor/feedback.git
+cd feedback
+
+
+
