269b2ab33d84ef9d844042913bb845b6dd18a9d0
Max Igor <maxigor.ferreira@gmail.com>
2022-06-21 17:54:54 -0300

    q

--- 03-paru.sh
+++ 03-paru.sh
@@ -16,26 +16,24 @@
 #-------------------------------------------------------------------------
 
 
-# AUR HELPER
 echo
 echo "INSTALLING AUR SOFTWARE"
 echo
 
+echo "Please enter username:"
+read username
+
 cd "${HOME}"
 
 echo "CLONING: PARU"
+git clone "https://aur.archlinux.org/paru.git"
 
-git clone https://aur.archlinux.org/paru.git
-
-cd {$HOME}/paru
-makepkg -si
 
 PKGS=(
 
     # UTILITIES -----------------------------------------------------------
-
-    'ly'						# ly display manager
-    #'i3lock-fancy'              # Screen locker
+    'ly'						# Display Manager
+    'i3lock-fancy'              # Screen locker
     'timeshift'                 # Backup and Restore
 
     # COMMUNICATIONS ------------------------------------------------------
@@ -48,23 +46,28 @@ PKGS=(
     'materia-gtk-theme'             # Desktop Theme
     'papirus-icon-theme'            # Desktop Icons
     'capitaine-cursors'             # Cursor Icons
-   
+    'autojump'                      # Zsh plugin
+
+    # APPS ----------------------------------------------------------------
+
+    'discord'                       # Chat for gamers
+    'youtubemusic-nativefier'        # Music
+
 )
 
+cd ${HOME}/paru
+makepkg -si
+
+# Change default shell
+chsh -s $(which zsh)
 
 for PKG in "${PKGS[@]}"; do
     paru -S --noconfirm $PKG
 done
 
-
-sudo systemctl enable ly.service
-
 echo
 echo "Done!"
 echo
-
-
-
 
 
 
