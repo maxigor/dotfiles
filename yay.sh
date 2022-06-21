13ebcd878539fe61f0524f0dfdb7c2918fb142ec
Max Igor <maxigor.ferreira@gmail.com>
2022-06-21 17:51:44 -0300

    yay

---
+++ yay.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+#-------------------------------------------------------------------------
+#      _          _    __  __      _   _
+#     /_\  _ _ __| |_ |  \/  |__ _| |_(_)__
+#    / _ \| '_/ _| ' \| |\/| / _` |  _| / _|
+#   /_/ \_\_| \__|_||_|_|  |_\__,_|\__|_\__|
+#  Arch Linux Post Install Setup and Config
+#-------------------------------------------------------------------------
+
+echo
+echo "INSTALLING AUR SOFTWARE"
+echo
+
+echo "Please enter username:"
+read username
+
+cd "${HOME}"
+
+echo "CLONING: YAY"
+git clone "https://aur.archlinux.org/yay.git"
+
+
+PKGS=(
+
+    # UTILITIES -----------------------------------------------------------
+
+    'i3lock-fancy'              # Screen locker
+    'timeshift'                 # Backup and Restore
+
+    # COMMUNICATIONS ------------------------------------------------------
+
+    'brave-bin'                 # Brave
+
+    # THEMES --------------------------------------------------------------
+
+    'lightdm-webkit-theme-aether'   # Lightdm Login Theme - https://github.com/NoiSek/Aether#installation
+    'materia-gtk-theme'             # Desktop Theme
+    'papirus-icon-theme'            # Desktop Icons
+    'capitaine-cursors'             # Cursor Icons
+    'autojump'                      # Zsh plugin
+
+    # APPS ----------------------------------------------------------------
+
+    'discord'                       # Chat for gamers
+    'youtubemusic-nativefier'        # Music
+
+)
+
+cd ${HOME}/yay
+makepkg -si
+
+# Change default shell
+chsh -s $(which zsh)
+
+for PKG in "${PKGS[@]}"; do
+    yay -S --noconfirm $PKG
+done
+
+echo
+echo "Done!"
+echo
