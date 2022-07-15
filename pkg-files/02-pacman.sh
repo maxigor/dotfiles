e27ab694c3e07a0cafebe125ead5c6d386e38ec2
Max <maxigor.ferreira@gmail.com>
2022-07-15 13:05:54 -0300

    eww

--- pkg-files/02-pacman.sh
+++ pkg-files/02-pacman.sh
@@ -184,10 +184,18 @@ done
     cd /home/max/Downloads
     echo "Cloning Picom Pijulius..."
     git clone https://github.com/pijulius/picom.git
-    cd /home/max/picom/
+    cd /home/max/Downloads/picom/
     meson --buildtype=release . build --prefix=/usr -Dwith_docs=true
     sudo ninja -C build install
 
+   # EWW -----------------------------------------------------------------
+
+   cd /home/max/Downloads
+   git clone https://github.com/elkowar/eww.git
+   cd /home/max/Downloads/eww
+   cargo build --release -j $(nproc)
+   cd target/release
+   sudo mv eww /usr/bin/eww
 
 echo
 echo "Done!"
