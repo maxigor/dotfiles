d510ee785577b3616ec31dc92c244decea264402
Max <maxigor.ferreira@gmail.com>
2022-07-18 21:21:24 -0300

    rere

--- pkg-files/04-post.sh
+++ pkg-files/04-post.sh
@@ -24,30 +24,30 @@
     done
 
     # PICOM PIJULIUS -----------------------------------------------------
-    cd ~/home/max/Downloads
+    cd /home/max/Downloads
     echo "Cloning Picom Pijulius..."
     git clone https://github.com/pijulius/picom.git
-    cd /home/max/Downloads/picom/
+    cd picom
     meson --buildtype=release . build --prefix=/usr -Dwith_docs=true
     sudo ninja -C build install
 
     # EWW -----------------------------------------------------------------
-    cd ~/home/max/Downloads
+    cd /home/max/Downloads
     git clone https://github.com/elkowar/eww.git
-    cd /home/max/Downloads/eww
+    cd eww
     cargo build --release -j $(nproc)
     cd target/release
     sudo mv eww /usr/bin/eww
 
     # XQP -----------------------------------------------------------------
-    cd ~/home/max/Downloads
+    cd /home/max/Downloads
     git clone https://github.com/baskerville/xqp.git
     cd xqp
     make
     sudo make install
 
     # XDO -----------------------------------------------------------------
-    cd ~/home/max/Downloads
+    cd /home/max/Downloads
     git clone https://github.com/baskerville/xdo
     cd xdo
     make
