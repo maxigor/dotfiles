e087e7503bbff8701682ac45cf729efcbb0f47fa
Max Igor <maxigor.ferreira@gmail.com>
2022-08-01 15:31:38 -0300

    font

--- scripts/0-preinstall.sh
+++ scripts/0-preinstall.sh
@@ -1,3 +1,4 @@
+
 #!/usr/bin/env bash
 #-------------------------------------------------------------------------
 #
@@ -7,7 +8,6 @@
 # ██╔══██║██╔══██╗██║     ██╔══██║██║╚██╔╝██║██╔══██║ ██╔██╗ 
 # ██║  ██║██║  ██║╚██████╗██║  ██║██║ ╚═╝ ██║██║  ██║██╔╝ ██╗
 # ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝
-#
 #-------------------------------------------------------------------------
 #github-action genshdoc
 #
@@ -33,8 +33,8 @@ source $CONFIGS_DIR/setup.conf
 iso=$(curl -4 ifconfig.co/country-iso)
 timedatectl set-ntp true
 pacman -S --noconfirm archlinux-keyring #update keyrings to latest to prevent packages failing to install
-# pacman -S --noconfirm --needed pacman-contrib terminus-font
-# setfont ter-v22b
+pacman -S --noconfirm --needed pacman-contrib terminus-font
+setfont ter-v12n
 sed -i 's/^#ParallelDownloads/ParallelDownloads/' /etc/pacman.conf
 pacman -S --noconfirm --needed reflector rsync grub
 cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
