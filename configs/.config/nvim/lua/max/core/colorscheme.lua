22e937d04b147a8e2216816e8caa481151026eae
Max <maxigor.ferreira@gmail.com>
2022-12-10 23:47:02 -0300

    some nvim colorscheme autoinstall

--- configs/.config/nvim/lua/max/core/colorscheme.lua
+++ configs/.config/nvim/lua/max/core/colorscheme.lua
@@ -1,2 +1,10 @@
+-- set colorscheme to nightfly with protected call
+-- in case it isn't installed
+local status, _ = pcall(vim.cmd, "colorscheme nightfly")
+if not status then
+	print("Colorscheme not found!") -- print error if colorscheme not installed
+	return
+end
+
 vim.g.nightflyTransparent = true
 vim.cmd("colorscheme nightfly")
