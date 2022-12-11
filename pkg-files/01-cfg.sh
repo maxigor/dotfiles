9a0c6fe7371485c090cadd1005877ef7a2f680ee
Max <maxigor.ferreira@gmail.com>
2022-12-10 23:37:28 -0300

    some perm and modifications

--- pkg-files/01-cfg.sh
+++ pkg-files/01-cfg.sh
@@ -14,21 +14,11 @@
 
 
 mkdir /home/max/Downloads
-mkdir /home/max/wallpaper
-
-cp /home/max/dotfiles/wall.jpg /home/max/wallpaper
 
 cp -r /home/max/dotfiles/configs/.config /home/max/
 cp /home/max/dotfiles/configs/.Xresources /home/max/
 cp /home/max/dotfiles/configs/.xinitrc /home/max/
 cp /home/max/dotfiles/configs/.zshrc /home/max/
-sudo cp /home/max/dotfiles/configs/fstab /etc/
-
-chmod +x /home/max/.config/bspwm/bspwmrc
-chmod +x /home/max/.config/sxhkd/sxhkdrc
-chmod +x /home/max/.config/.fehbg
-chmod +x /home/max/.config/polybar/launch.sh
-
 
 # git config
 git config --global user.email "maxigor.ferreira@gmail.com"
